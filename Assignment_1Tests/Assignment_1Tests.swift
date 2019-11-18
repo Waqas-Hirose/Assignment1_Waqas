641fb59f3ef60a88481971fa1b48699557ba2b6f
Waqas <w.ahmad@appdevream.work>
2019-11-18 08:49:37 +0500

    Initial commit

---
+++ Assignment_1Tests/Assignment_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Assignment_1Tests.swift
+//  Assignment_1Tests
+//
+//  Created by TK-LPT-0142 on 11/15/19.
+//  Copyright © 2019 Waqas. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment_1
+
+class Assignment_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
